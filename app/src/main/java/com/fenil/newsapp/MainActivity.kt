0d2d6df1146a50cb688980d374c7e56a853b16ab
Fenil Moradiya <fjmoradiya@gmail.com>
2024-04-20 03:24:13 +0530

    Project Setup

---
+++ app/src/main/java/com/fenil/newsapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.fenil.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.fenil.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
